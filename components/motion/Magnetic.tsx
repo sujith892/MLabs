856ed59d59cc3579f9d697c3d30effac650a886f
sujith892 <61357592+sujith892@users.noreply.github.com>
2026-08-29 12:39:09 +0530

    Clean up Magnetic component by removing imports and interface
    Removed unused imports and interface from Magnetic component.

--- components/motion/Magnetic.tsx
+++ components/motion/Magnetic.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { gsap } from "@/lib/gsap";
-
-interface MagneticProps {
-  children: React.ReactElement;
-  strength?: number;
-}
 
 
 export default function Magnetic() {
